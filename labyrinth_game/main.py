b30fbe040c2a8ea55dada34d06d8343a58143c33
RomanchukRoman <r.romanchuk@ya.ru>
2025-10-18 17:49:29 +0300

    add new func

--- labyrinth_game/main.py
+++ labyrinth_game/main.py
@@ -10,16 +10,53 @@ game_state = {
         'steps_taken': 0 # Количество шагов
   }
 
+# Основная игровая функция
 def main():
     print('\nДобро пожаловать в Лабиринт сокровищ!')
     utils.describe_current_room(game_state)
 
-# нужен while
-# этот код под вопросом
-    command = player_actions.get_input()
-    print(f'Вы ввели команду: {command}')
-  
+    # Цикл запрашивающий команду пока не введено - quit или exit
+    while not game_state['game_over']:
+        # Вызов функции пользовательского ввода команды 
+        command = player_actions.get_input()
+        process_command(game_state, command)
+
+
+def process_command(game_state, command):
+    '''
+    Обработка команд.
+    Функция process_command должна принимать game_state и введенную пользователем строку.
+    Внутри нее разделите строку на части, чтобы отделить команду от аргумента (например, 'go north' -> 'go', 'north').
+    Используя match / case, определите, какую команду ввел пользователь (look, use, go, take, inventory, quit).
+    Вызовите соответствующую функцию (describe_current_room, move_player, take_item и т.д.) в рамках условия, передав ей нужный аргумент.
+    В цикле while в функции main() вызывайте process_command для каждой введенной пользователем строки. Убедитесь, что команда quit или exit завершает игру.
+    '''
+    parts = command.split()
+    # первое слово — команда
+    action = parts[0]
+    arg = parts[1:]
+
+    match action:
+        case 'look':
+            utils.describe_current_room(game_state)
+        case 'use':
+            print('use')
+        case 'go':
+            if arg:
+                direction = arg[0]
+                player_actions.move_player(game_state, direction)
+        case 'take':
+            print('take')
+        case 'inventory':
+            player_actions.show_inventory(game_state)
+        case 'quit':
+            print('\nВыход из игры.\n')
+            game_state['game_over'] = True
+        case 'exit':
+            print('\nВыход из игры.\n')
+            game_state['game_over'] = True
 
 if __name__ == '__main__':
     main()
-  
+
+# poetry run project
--- labyrinth_game/player_actions.py
+++ labyrinth_game/player_actions.py
@@ -1,5 +1,8 @@
 # labyrinth_game/player_actions.py
 
+from labyrinth_game.constants import ROOMS
+from labyrinth_game import utils
+
 def show_inventory(game_state):
     '''
     Функция отображения инвентаря.
@@ -9,8 +12,10 @@ def show_inventory(game_state):
     inventory = game_state['player_inventory']
     if inventory:
         print(', '.join(inventory))
+    else:
+        print('\nВаш инвентарь пуст.')
 
-def get_input(prompt="> "):
+def get_input(prompt='\nВведите команду > '):
     '''
     Ввод пользователя.
     Так как мы часто будем запрашивать данные у пользователя стоит вынести эту логику в отдельную функцию.
@@ -22,3 +27,26 @@ def get_input(prompt="> "):
     except (KeyboardInterrupt, EOFError):
         print("\nВыход из игры.")
         return "quit"
+    
+def move_player(game_state, direction):
+    '''
+    Функция перемещения.
+    Она должна принимать два аргумента — game_state и направление (строку, например, 'north').
+    Проверяет, существует ли выход в этом направлении.
+    Если выход есть:
+    Обновите текущую комнату.
+    Увеличьте шаг на единицу.
+    Выведите описание новой комнаты.
+    Если выхода нет, выведите на экран сообщение: "Нельзя пойти в этом направлении."
+    '''
+    room = game_state['current_room']
+    room_data = ROOMS[game_state['current_room']]
+    exits = room_data['exits']
+
+    if direction in exits.keys():
+        game_state['current_room'] = exits[direction]
+        game_state['steps_taken'] += 1
+
+        utils.describe_current_room(game_state)
+    else: 
+        print('Нельзя пойти в этом направлении.')
